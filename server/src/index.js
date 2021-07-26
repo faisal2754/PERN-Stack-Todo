443cf11bb6d5eb928a7dc3f819646eb3f7d2122f
Faisal <saleemfs27@gmail.com>
2021-07-26 18:22:27 +0200

    Clean up

--- server/src/index.js
+++ server/src/index.js
@@ -10,22 +10,6 @@ app.use(express.json())
 // Routes
 app.use('/todos', router)
 
-// const main = async () => {
-//   const newTodo = await prisma.todo.create({
-//     data: {
-//       description: 'Learn Prisma'
-//     }
-//   })
-//   console.log('New todo added: ', newTodo)
-
-//   const allTodos = await prisma.todo.findMany()
-//   console.log('All todos: ', allTodos)
-// }
-
-// main()
-//   .catch((e) => console.log(e))
-//   .finally(async () => await prisma.$disconnect())
-
 app.listen(5000, () => {
   console.log('server started on port 5000')
 })
