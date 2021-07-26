3b42dc99a93fa5eb03c573d1c24fab8ac917e1c8
Faisal <saleemfs27@gmail.com>
2021-07-26 20:06:16 +0200

    Added some queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,35 @@
+const { PrismaClient } = require('@prisma/client')
 const { gql } = require('apollo-server-express')
 
+const prisma = new PrismaClient()
+
 const typeDefs = gql`
+  type Todo {
+    todo_id: ID!
+    description: String!
+  }
+
   type Query {
     bruh: String
+    todos: [Todo]
+    todo: Todo
   }
 `
 const resolvers = {
   Query: {
-    bruh: () => 'lmao'
+    bruh: () => 'lmao',
+    todos: async () => {
+      const allTodos = await prisma.todo.findMany()
+      return allTodos
+    },
+    todo: async (_, { todo_id }) => {
+      const todo = await prisma.todo.findUnique({
+        where: {
+          todo_id: Number(todo_id)
+        }
+      })
+      return todo
+    }
   }
 }
 
